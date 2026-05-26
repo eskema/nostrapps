6159e7963034b7b0ad350470f2611db5cab8456a
fiatjaf <fiatjaf@gmail.com>
2026-05-25 22:18:40 -0300

    update pomegranate operator list.

--- src/signers/google.ts
+++ src/signers/google.ts
@@ -5,7 +5,7 @@ import { bytesToHex } from "@noble/hashes/utils.js"
 
 // Hardcoded Pomegranate deployment we point this launcher at.
 const CENTRAL_URL = "https://auth.njump.me"
-const OPERATORS = ["https://po.njump.me", "https://po.fiatjaf.com"]
+const OPERATORS = ["https://po.njump.me", "https://po.nostrver.se", "https://po.f7z.io"]
 
 const utf8_encode = (s: string) => new TextEncoder().encode(s)
 
